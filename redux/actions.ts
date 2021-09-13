90c0f03ae1a51fd7bb71458757676f6c76a256dc
Mfundo Shabalala <mfundoshabalala@gmailcom>
2021-09-13 10:23:18 +0200

    redux actions

---
+++ redux/actions.ts
@@ -0,0 +1,8 @@
+/*
+ * Actions for the todo list
+ */
+
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
