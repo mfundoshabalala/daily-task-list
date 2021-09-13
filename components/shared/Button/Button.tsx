3b1c6ce67433b85444e7f766541b5931bb8b9b7f
Mfundo Shabalala <mfundoshabalala@gmailcom>
2021-09-14 01:34:06 +0200

    Button.tsx update

--- components/shared/Button/Button.tsx
+++ components/shared/Button/Button.tsx
@@ -1,19 +1,34 @@
+import classNames from 'classnames';
+
 interface ButtonProps {
-	title: string;
+	title?: string;
 	onClick: () => void;
 	className?: string;
 	disabled?: boolean;
 	type?: 'button' | 'submit' | 'reset';
+	icon?: string;
+	optionalIcon?: string;
+	optionalCriteria?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ title, onClick }) => (
-	<button
-		type="button"
-		onClick={onClick}
-		className="px-2 py-1 text-sm font-light border rounded"
-	>
-		{title}
-	</button>
-);
+const Button: React.FC<ButtonProps> = (props) => {
+	const { title, onClick, className, icon, optionalIcon, optionalCriteria } = props;
+	return (
+		<button
+			type="button"
+			onClick={onClick}
+			className={classNames(
+				className,
+				'px-2 py-1 border rounded drop-shadow-md active:drop-shadow-sm'
+			)}
+		>
+			{title}
+			<svg className="w-6 h-6 transition-all duration-300 hover:scale-105 currentColor">
+				{!optionalCriteria && <use xlinkHref={`/sprite.svg#${icon}`} />}
+				{optionalCriteria && <use xlinkHref={`/sprite.svg#${optionalIcon}`} />}
+			</svg>
+		</button>
+	);
+};
 
 export default Button;
