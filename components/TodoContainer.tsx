7c559803970ca5082eba23d4449855ba85ddbdd4
Mfundo Shabalala <mfundoshabalala@gmailcom>
2021-09-15 17:34:15 +0200

    adding footer component

--- components/TodoContainer.tsx
+++ components/TodoContainer.tsx
@@ -1,10 +1,12 @@
 import TodoList from 'components/TodoList';
 import TodoHeader from 'components/TodoHeader';
+import Footer from './Footer';
 
 const TodoContainer: React.FunctionComponent = () => (
 	<div className="container flex flex-col items-center justify-start h-screen gap-6">
 		<TodoHeader />
 		<TodoList />
+		<Footer />
 	</div>
 );
 
