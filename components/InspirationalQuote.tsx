5146581b44227142c83373ab4d4421c91b4ed85d
Mfundo Shabalala <mfundoshabalala@gmailcom>
2021-09-15 22:18:58 +0200

    inspirational quote component init commit

---
+++ components/InspirationalQuote.tsx
@@ -0,0 +1,21 @@
+import useFetchData from 'hooks/useFetchData';
+
+const InspirationalQuote: React.FC = () => {
+	const { data, error } = useFetchData();
+
+	const quote = data?.contents.quotes[0].quote;
+	const author = data?.contents.quotes[0].author;
+
+	return (
+		<>
+			{error && <div>Error!</div>}
+			{!error && (
+				<q className="text-sm italic text-center text-gray-600 font-product-sans drop-shadow">
+					{quote} - {author}
+				</q>
+			)}
+		</>
+	);
+};
+
+export default InspirationalQuote;
