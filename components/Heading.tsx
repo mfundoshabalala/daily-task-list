bfb626176fb2f6c0338ef1bfce52c90d50d52004
Mfundo Shabalala <mfundoshabalala@gmailcom>
2021-09-15 22:16:18 +0200

    Heading.tsx init commit

---
+++ components/Heading.tsx
@@ -0,0 +1,7 @@
+const Heading: React.FC = () => (
+	<h1 className="py-2 text-4xl font-black text-center text-transparent font-product-sans drop-shadow bg-gradient-to-r from-blue-500 to-blue-500 via-green-500 bg-clip-text">
+		Daily Task List
+	</h1>
+);
+
+export default Heading;
