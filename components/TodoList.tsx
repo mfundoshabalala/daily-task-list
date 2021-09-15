b691f7613fa04dcc4cdfd5ce9ba27768a2153183
Mfundo Shabalala <mfundoshabalala@gmailcom>
2021-09-15 17:33:05 +0200

    Adding empty list placeholder

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,20 +1,22 @@
 import _ from 'lodash';
-import { useSelector } from 'react-redux';
 import { RootState } from 'redux/reducer';
-//
-import TodoItem from 'components/TodoItem';
+import { useSelector } from 'react-redux';
+// hooks
 import useMounted from 'hooks/useMounted';
+// components
+import TodoItem from 'components/TodoItem';
+import EmptyTodoList from './EmptyTodoList';
 
 const TodoList: React.FunctionComponent = () => {
 	const mounted = useMounted();
 	const todos = useSelector((state: RootState) => state.todos);
 
 	return (
-		<ul className="flex flex-col items-center w-full gap-4 px-4">
+		<ul className="relative flex flex-col items-center w-full h-full gap-4 px-4">
 			{mounted &&
 				todos &&
 				_.map(todos, (todo) => <TodoItem key={todo?.taskId} todo={todo} />)}
-			{mounted && !todos.toString() && <p className="text-center">No todos</p>}
+			{mounted && !todos.toString() && <EmptyTodoList />}
 		</ul>
 	);
 };
